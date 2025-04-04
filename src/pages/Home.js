196a3bc98aaf3a6fa3366ed724a43f1374008022
KatVolkova <volkovakatrin87@yahoo.com>
2025-04-04 11:20:39 +0100

    Install routing and update App.js for React Router

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>Welcome to Study Cards Home Page</h1>;
+};
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,6 @@
+const Login = () => {
+    return <h1>Log In Page</h1>;
+  };
+  
+  export default Login;
+  
